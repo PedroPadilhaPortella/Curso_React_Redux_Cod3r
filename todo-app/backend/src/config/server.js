dcf20c6c4f08786fce6b5258f4a988fd18fb7a8f
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2023-07-30 23:28:10 -0300

    projeto todoApp - backend

---
+++ todo-app/backend/src/config/server.js
@@ -0,0 +1,13 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const allowCors = require('./cors')
+const server = express()
+const port = 3003
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(allowCors)
+
+server.listen(port, () => console.log(`BACKEND is running on port ${port}.`));
+
+module.exports = server
