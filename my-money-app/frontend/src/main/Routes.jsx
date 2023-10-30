42f2d6fe07bd3d90ccd0f2e448b181c54249b6ce
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2023-10-30 01:00:44 -0300

    feat: Autenticação no Frontend

--- my-money-app/frontend/src/main/Routes.jsx
+++ my-money-app/frontend/src/main/Routes.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { IndexRoute, Redirect, Route, Router, hashHistory } from 'react-router'
 import BillingCycle from '../billingCycle/BillingCycle'
 import Dashboard from '../dashboard/Dashboard'
-import App from './app'
+import AuthOrApp from './AuthOrApp'
 
 export default (props) => (
   <Router history={hashHistory}>
-    <Route path='/' component={App}>
+    <Route path='/' component={AuthOrApp}>
       <IndexRoute component={Dashboard} />
       <Route path='billingCycles' component={BillingCycle} />
     </Route>
--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -4,6 +4,7 @@ import TabReducer from '../common/tab/TabReducer'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 import BillingCycleReducer from '../billingCycle/BillingCycleReducer'
+import AuthReducer from '../auth/authReducer'
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
   billingCycle: BillingCycleReducer,
   form: formReducer,
   toastr: toastrReducer,
+  auth: AuthReducer,
 })
 
 export default rootReducer
